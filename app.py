62fdb6ac61d6d5fe1ff14edd1886ea99d90d4345
Derek Eder <derek.eder@gmail.com>
2015-01-04 22:37:54 -0600

    added crossdomin support for api queries

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, make_response, render_template
 from datetime import date, datetime, timedelta
+from functools import update_wrapper
 import pytz
 import json
 import requests
@@ -8,7 +9,49 @@ import os
 
 app = Flask(__name__)
 
+def crossdomain(origin=None, methods=None, headers=None,
+                max_age=21600, attach_to_all=True,
+                automatic_options=True): # pragma: no cover
+    if methods is not None:
+        methods = ', '.join(sorted(x.upper() for x in methods))
+    if headers is not None and not isinstance(headers, basestring):
+        headers = ', '.join(x.upper() for x in headers)
+    if not isinstance(origin, basestring):
+        origin = ', '.join(origin)
+    if isinstance(max_age, timedelta):
+        max_age = max_age.total_seconds()
+
+    def get_methods():
+        if methods is not None:
+            return methods
+
+        options_resp = app.make_default_options_response()
+        return options_resp.headers['allow']
+
+    def decorator(f):
+        def wrapped_function(*args, **kwargs):
+            if automatic_options and request.method == 'OPTIONS':
+                resp = current_app.make_default_options_response()
+            else:
+                resp = make_response(f(*args, **kwargs))
+            if not attach_to_all and request.method != 'OPTIONS':
+                return resp
+
+            h = resp.headers
+
+            h['Access-Control-Allow-Origin'] = origin
+            h['Access-Control-Allow-Methods'] = get_methods()
+            h['Access-Control-Max-Age'] = str(max_age)
+            if headers is not None:
+                h['Access-Control-Allow-Headers'] = headers
+            return resp
+
+        f.provide_automatic_options = False
+        return update_wrapper(wrapped_function, f)
+    return decorator
+
 # ROUTES
+@crossdomain(origin="*")
 @app.route('/plow-tracker-is-on/')
 def plow_tracker_is_on():
     plow_page = requests.get('https://gisapps.cityofchicago.org/PlowTrackerWeb/PlowTrackerAccess')
@@ -24,6 +67,7 @@ def plow_tracker_is_on():
     resp.headers['Content-Type'] = 'application/json'
     return resp
 
+@crossdomain(origin="*")
 @app.route('/snow-plow-data/')
 def snow_plow_data():
 
@@ -39,7 +83,6 @@ def snow_plow_data():
 
         try:
           read_data = response.json()['TrackingDataResponse']['locationList']
-          print read_data
           asset_types = set([a['assetType'] for a in read_data])
           
           for asset in asset_types:
